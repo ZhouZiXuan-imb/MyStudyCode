2b15181fc4879272b5706b260ff206f8451090f6
ZhouZiXuan-imb <1364421737@qq.com>
2020-11-06 08:51:51 +0800

    happy-answer项目修改

---
+++ Vue3.0学习资料/composition API/composition-api-setup/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <!--  <vSetup msg="周子轩"></vSetup>-->
+  <!--  <vRef></vRef>-->
+  <vReactive></vReactive>
+</template>
+
+<script>
+// import vSetup from "@/components/vSetup";
+// import vRef from "@/components/vRef";
+import vReactive from "@/components/vReactive";
+
+export default {
+  name: "App",
+  components: {
+    // vSetup,
+    // vRef,
+    vReactive
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
